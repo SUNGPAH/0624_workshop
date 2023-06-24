07d14493ed9424609171f5905f3cb2ca33f31cdc
sungpah <sungpah@gmail.com>
2023-06-24 18:50:00 +0900

    Update

--- pages/memoList.js
+++ pages/memoList.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-
 export default function MemoList() {
   const router = useRouter();
-  const [email, setEmail] = useState("")
   const memoList = [
+    {
+      id: 1,
+      createdAt: "2023..",
+      title: "title", 
+      body: "~",
+    },
+    {
+      id: 1,
+      createdAt: "2023..",
+      title: "title", 
+      body: "~",
+    },
+    {
+      id: 1,
+      createdAt: "2023..",
+      title: "title", 
+      body: "~",
+    },
     {
       id: 1,
       createdAt: "2023..",
@@ -18,17 +34,31 @@ export default function MemoList() {
     router.push("/memo/" + id)
   }
 
+  const containerStyle = {
+    flex:1,
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '10px' // Adjust the gap between items as desired
+  };
+
+  const itemStyle = {
+    display: "flex",
+    padding:4,
+    backgroundColor:'pink',
+    height:10,
+  }
+
   return (
-    <div>
+    <div style={{display:'flex',}}>
+      <div style={containerStyle}>
       {memoList.map((memo, index) => {
-        return <div key={memo.id}>
-          
+        return <div key={memo.id} style={itemStyle}>
           <div onClick={e => moveToMemo(memo.id)}>
             {memo.title}
           </div>
-
         </div>
       })}
+      </div>
     </div>
   )
 }
